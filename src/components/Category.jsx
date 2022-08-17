56168217005c1cd000b3fff097e0c03a444917d7
EmirAkimzhanov <emir.akimzhanov@gmail.com>
2022-08-17 16:25:59 +0600

    catalog and search input added with logic

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -12,10 +12,40 @@ import { useSearchParams } from "react-router-dom";
 import { useProducts } from "../contexts/ProductContextProvider";
 import Popper from "@mui/material/Popper";
 import Fade from "@mui/material/Fade";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import SearchIcon from "@mui/icons-material/Search";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
 
 const Category = () => {
   const { getProducts, fetchByParams } = useProducts();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   const [search, setSearch] = useState(searchParams.get("q") || "");
 
@@ -38,50 +68,103 @@ const Category = () => {
 
   const canBeOpen = open && Boolean(anchorEl);
   const id = canBeOpen ? "transition-popper" : undefined;
+  const settings = ["All", "Clothes", "Sport", "Cars"];
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: "20ch",
+      },
+    },
+  }));
 
   return (
     <div>
-      <button aria-describedby={id} type="button" onClick={handleClick}>
-        Toggle Popper
-      </button>
-      <Popper id={id} open={open} anchorEl={anchorEl} transition>
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={350}>
+      <Box
+        sx={{ flexGrow: 0, p: 5, display: "flex", justifyContent: "center" }}
+      >
+        <Tooltip title="Open settings">
+          <Button
+            className="btn-catalog"
+            onClick={handleOpenUserMenu}
+            sx={{ p: 0 }}
+          >
+            <span>catalog</span>
+          </Button>
+        </Tooltip>
+
+        <input
+          className="inpSearch"
+          placeholder="🔎Search..."
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting) => (
             <Box
-              defaultValue="all"
-              sx={{
-                border: 1,
-                p: 1,
-                bgcolor: "background.paper",
-                display: "flex",
-                flexDirection: "column",
-              }}
+              key={setting}
+              onClick={handleCloseUserMenu}
+              sx={{ width: "10vw" }}
             >
               <Button
-                value="all"
+                value={setting.toLowerCase()}
                 onClick={(e) => fetchByParams("category", e.target.value)}
+                textAlign="center"
               >
-                ALL
-              </Button>
-              <Button
-                value="sport"
-                onClick={(e) => fetchByParams("category", e.target.value)}
-              >
-                sport
-              </Button>
-              <Button
-                value="clothes"
-                onClick={(e) => {
-                  fetchByParams("category", e.target.value);
-                  handleClick();
-                }}
-              >
-                clothes
+                {setting}
               </Button>
             </Box>
-          </Fade>
-        )}
-      </Popper>
+          ))}
+        </Menu>
+      </Box>
     </div>
   );
 };
