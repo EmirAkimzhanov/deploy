ec8394fc435e7b90153ab66e59abd4c7a1209bb3
EmirAkimzhanov <emir.akimzhanov@gmail.com>
2022-08-17 22:49:34 +0600

    started home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import {
+  MDBCarousel,
+  MDBCarouselInner,
+  MDBCarouselItem,
+  MDBCarouselElement,
+  MDBCarouselCaption,
+} from "mdb-react-ui-kit";
+import { Paper } from "@mui/material";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
+export default function HomePage() {
+  return (
+    <Paper
+      elevation={0}
+      sx={{
+        width: "90%",
+        margin: "0 auto",
+        marginTop: "2%",
+      }}
+    >
+      <MDBCarousel interval={2000} showIndicators dark fade>
+        <MDBCarouselInner style={{ borderRadius: "5px" }}>
+          <MDBCarouselItem className="active">
+            <MDBCarouselElement
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(19).webp"
+              alt="..."
+            />
+            <MDBCarouselCaption></MDBCarouselCaption>
+          </MDBCarouselItem>
+          <MDBCarouselItem>
+            <MDBCarouselElement
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(35).webp"
+              alt="..."
+            />
+            <MDBCarouselCaption></MDBCarouselCaption>
+          </MDBCarouselItem>
 
-export default HomePage;
+          <MDBCarouselItem>
+            <MDBCarouselElement
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(40).webp"
+              alt="..."
+            />
+            <MDBCarouselCaption></MDBCarouselCaption>
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </Paper>
+  );
+}
